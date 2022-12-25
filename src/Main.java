61b487e5b96efb0198d30f075072507c6cfe35d6
Aslıhan <aslihan_hasar@hotmail.com>
2022-12-25 19:51:39 +0300

    Add Main class codes via upload

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Fighter fighterOne = new Fighter("Thunder", 90, 25, 120, 60);
+        Fighter fighterTwo = new Fighter("Nightmare", 80, 20, 130, 60);
+        Ring match = new Ring(fighterOne, fighterTwo, 70, 100);
+        match.run();
+    }
+}
